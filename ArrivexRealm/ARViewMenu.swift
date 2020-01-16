f57a512f15cedaf6ff64472a9cd6f705682d0302
Pasinee Srisuwongse <ff_nat@hotmail.com>
2020-01-16 23:39:26 +0700

    hidden menu w/toggle ar

--- ArrivexRealm/ARViewMenu.swift
+++ ArrivexRealm/ARViewMenu.swift
@@ -12,24 +12,33 @@ struct ARViewMenu: View {
     @State var showmenu = true
     var body:  some View {
         
-    
-       
+  
         
-         ZStack{ // set frame of menus bar
+    ZStack{ // set frame of menus bar
         
-          ZStack{
+        VStack{
+            
+             Button(action : { withAnimation{ self.showmenu.toggle() }} ) {
+                            Image(systemName: "text.justify")
+            //                Image(systemName: "line.horizontal.3")
+                                .font(.title).foregroundColor(.blue).padding(15).padding(.horizontal,15)
+             }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
+                
+            ZStack() {
+            ZStack() {
                 
             RoundedRectangle(cornerRadius: 25).foregroundColor(Color.white).opacity(0.3)
 
             VStack{
                 
             Button(action:{
+                print("ac")
             }) {
                 VStack{
                     Image(systemName: "faceid").font(.largeTitle).shadow(radius: 1.0,x: 2, y: 2)
                     Text("ACHIEVE").font(.footnote).fontWeight(.ultraLight)
-                }
-            }.padding(.all,8.0)
+                }.padding(.all,8.0)
+            }.padding(.top,8.0)
      
             
             Button(action:{
@@ -43,7 +52,7 @@ struct ARViewMenu: View {
             }.padding(.all,8.0)
             
             Button(action:{
-            
+                print("screen")
             }) {
                 VStack{
                 Image(systemName: "camera.viewfinder").font(.largeTitle).shadow(radius: 1.0,x: 2, y: 2)
@@ -54,19 +63,24 @@ struct ARViewMenu: View {
             }.padding(.all,8.0)
                 
             Button(action:{
+                print("stamp")
             }) {
                 VStack{
                     Image(systemName: "book").font(.largeTitle).shadow(radius: 1.0,x: 2, y: 2)
                     Text("STAMPS").font(.footnote).fontWeight(.ultraLight)
-                }
-            }.padding(.all,8.0)
+                }.padding(.all,8.0)
+            }.padding(.bottom,8.0)
             
             }.foregroundColor(Color(.blue))
                 
-          }.frame(maxWidth: 73, maxHeight: 350)
+//          }.frame(maxWidth: 73, maxHeight: 360).offset(x : showmenu ? 0: UIScreen.main.bounds.height)
+            }.frame(maxWidth: 73, maxHeight: 360).offset(x : showmenu ? 0: UIScreen.main.bounds.height)
+            }.frame(maxWidth: .infinity, maxHeight: .infinity,alignment: .bottomTrailing).padding(15).padding(.bottom,50)
+        
+        }//Vstack
 
             
-         }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing).padding(.all,12).padding(.bottom, 60)
+    }.frame(maxWidth: .infinity, maxHeight: .infinity).background(Color(.black)).edgesIgnoringSafeArea(.bottom)
 
         
 
