aee68c5f75969a7242062726f1000d9fdfc24730
Pasinee Srisuwongse <ff_nat@hotmail.com>
2020-01-26 17:35:07 +0700

    Discovery with no respond

--- ArrivexRealm/SelectLocation.swift
+++ ArrivexRealm/SelectLocation.swift
@@ -13,8 +13,9 @@ import MapKit
 struct SelectLocation: View {
     
     @State private var isDiscovery = false
- 
-
+    @State private var isActiveARView = false
+    @EnvironmentObject var data: Datamodel
+    
     var body: some View {
         
         ZStack {
@@ -37,32 +38,75 @@ struct SelectLocation: View {
                    
                 ZStack{
                     
-                    MapView().padding(10).navigationBarTitle(Text("Discovery"))
+                    MapView().padding(10).navigationBarTitle(Text("Discovery")).navigationBarItems(leading:
+                         HStack {
+                               Button("Back") {
+                                 self.isActiveARView = true
+                               }
+                             
+                           }
+//                      help button ทำ tutarial
+//                      ,
+//                    trailing:
+//                        HStack {
+//
+//                               Button("Help") {
+//                                   print("Help tapped!")
+//                               }
+//                           }
+                        
+                    )
               
                     ZStack{
 //                        ZStack{
                         ScrollView(.horizontal) {
                         HStack{
-                            Image("stamp").resizable().frame(width: 100,height: 160)
+                            VStack{
+                            Image("Stamp").resizable().frame(width: 110,height: 160)
                                                        .overlay(
                                                           Rectangle()
                                                        .fill(LinearGradient(gradient: Gradient(colors: [.clear]),
                                                              startPoint: .center, endPoint: .bottom)))
                                                        .cornerRadius(12.0).shadow(radius: 1.5,x: 2, y: 2)
+                                Text("Mea Sue Pavilion").font(.footnote).fontWeight(.regular).padding(3).background(Color(.white)).cornerRadius(12.0)
+                            }
                                 
-                            
-                            Image("stamp1").resizable().frame(width: 100,height: 160)
+                            VStack{
+                            Image("Stamp1").resizable().frame(width: 110,height: 160)
                                 .overlay(
                                    Rectangle()
                                 .fill(LinearGradient(gradient: Gradient(colors: [.clear]),
                                       startPoint: .center, endPoint: .bottom)))
                                 .cornerRadius(12.0).shadow(radius: 1.5,x: 2, y: 2)
+                                Text("Nuad Pavilion").font(.footnote).fontWeight(.regular).padding(3).background(Color(.white)).cornerRadius(12.0)
+                            }
+                            
+                            
+                            VStack{
+                            Image("Stamp2").resizable().frame(width: 110,height: 160)
+                                                       .overlay(
+                                                          Rectangle()
+                                                       .fill(LinearGradient(gradient: Gradient(colors: [.clear]),
+                                                             startPoint: .center, endPoint: .bottom)))
+                                                       .cornerRadius(12.0).shadow(radius: 1.5,x: 2, y: 2)
+                                Text("Reclining Buddha").font(.footnote).fontWeight(.regular).padding(3).background(Color(.white)).cornerRadius(12.0)
+                            }
+                            
+                            VStack{
+                            Image("Stamp3").resizable().frame(width: 110,height: 160)
+                                .overlay(
+                                   Rectangle()
+                                .fill(LinearGradient(gradient: Gradient(colors: [.clear]),
+                                      startPoint: .center, endPoint: .bottom)))
+                                .cornerRadius(12.0).shadow(radius: 1.5,x: 2, y: 2)
+                                Text("Misakawan Park").font(.footnote).fontWeight(.regular).padding(3).background(Color(.white)).cornerRadius(12.0)
+                            }
                                 
-                        }
-//                        }.padding(.horizontal,20).padding(.bottom,10)
+                        }.foregroundColor(Color(.black))
+
                          
                             
-                        }.padding(.horizontal,20).padding(.bottom,10)
+                        }.padding(.horizontal,20).padding(.bottom,8)
                     
                     }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomLeading).padding(.bottom,10)
     
@@ -80,25 +124,25 @@ struct SelectLocation: View {
                                       }.background(Color(.black)).cornerRadius(30)
                 }
               }
-//                Button(action:{
-//                    self.isDiscovery = true
-//                        print("Dicovery")
-//                        }) {
-//                    HStack {
-//                        Image(systemName: "magnifyingglass")
-//                        Text("Discovery").fontWeight(.semibold)
-//                        }.padding(10).foregroundColor(.white)
-//                    }.background(Color(.black)).cornerRadius(30)
-                
-                
-//            }.padding(.bottom,10) //V
-              
-               
+
                 
             }.frame(maxWidth: .infinity, maxHeight: .infinity).background(Color(.cream))
             if isDiscovery == true {
                 DiscoveryPlaces()
                }
+            
+            if isActiveARView == true {
+                ZStack {
+                    if data.enableAR {ARDisplayView()}
+                    
+                    else {
+                        Color(.cream).edgesIgnoringSafeArea(.all)
+                    }
+                    
+                    
+                    ARViewMenu()
+                }.frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
                          
         } //z
      
